3ba108b5ef7f14fad93dcd92c65a7a96d9517433
janinevallente <janinexvallente@gmail.com>
2026-04-13 20:09:08 +0800

    fix: resolved favicion display issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,11 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=DM+Sans:ital,wght@0,300;0,400;0,500;0,600;1,400&display=swap',
         },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+        },
       ],
     },
   },
